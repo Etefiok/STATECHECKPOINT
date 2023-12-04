049d7486045a4520c70fd9c96c0c7d7d25d42744
etefiok <diligentsamuel@gmail.com>
2023-12-04 21:35:08 +0100

    all new files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
+import { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  state = {
+    fullName: "diligent samuel",
+    bio: "this is my first state in react",
+    imgSrc: "https://www.mykhel.com/thumb/190x90x190/football/players/3/186703.1528888991.jpg ",
+    profession: "web Developer",
+    show: false
+  };
+
+     changeName(){
+    this.setState(prevState =>({
+      show: !prevState.false
+    }))
+     }
+
+   render(){
+    return(
+      <div>
+       <h2>My name is {this.state.fullName}</h2>
+       <p>{this.state.bio}</p>
+       <img src={this.state.imgSrc} />
+       <h4>my profession is {this.state.profession}</h4>
+       <button onClick={this.changeName}> change my name</button>
+      </div>
+    )
+  }
 }
 
 export default App;
